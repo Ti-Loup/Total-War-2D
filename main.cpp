05be5d62faeeae4217aa97f9d40beff05f6ea380
Louis <lpgauvin36@gmail.com>
2026-04-17 15:53:10 -0400

    Update Main

--- main.cpp
+++ main.cpp
@@ -1,16 +1,97 @@
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+//La librairie
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <SDL3_mixer/SDL_mixer.h>
+//les classes
+#include "State.h"
+#include "Entity.h"
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+class GameApp final {
+public:
+    SDL_Window *window = nullptr;
+    SDL_Renderer *renderer = nullptr;
+    SDL_Texture *spritesheet = nullptr;
+    //Ajout du state Menu
+    State StateActuel = State::Menu;
+
+    //TEXT ET FONT
+    // -> MENU <-
+
+    // -> OPTION <-
+
+    // -> GAME <-
+
+    // -> AUTRE <-
+    TTF_Font *fpsFont = nullptr;
+    TTF_Text *fpsText = nullptr;
+
+
+    //Pour le FPS
+    std::vector<float> frameTimes;
+    const size_t MAX_SAMPLES = 100;
+    bool shouldUpdateText = false;
+    SDL_TimerID fpsTimerID;
+    SDL_TimerID ShootTimerID;
+    float currentFPS = 0.0f;
+
+    float deltaTime = 0.f;
+
+
+    //PATRON DE CONCEPTION SINGLETON
+    static GameApp &GetInstance() {
+        static GameApp instance;
+        return instance;
     }
 
-    return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
-}
+private:
+    GameApp() {
+        //window + renderer
+        window = SDL_CreateWindow("Total Battle 2D",1280,720,0);
+        if (window == nullptr) {
+            SDL_LogCritical(1,"SDL failed to create the window! &s", SDL_GetError());
+            abort();
+        }
+        renderer = SDL_CreateRenderer(window, nullptr);
+        if (renderer == nullptr) {
+            SDL_LogCritical(1,"SDL failed to create the renderer &s", SDL_GetError());
+            abort();
+        }
+        //Pour mettre en fullscreen
+        SDL_SetRenderLogicalPresentation(renderer, 1920,1080, SDL_LOGICAL_PRESENTATION_LETTERBOX)
+
+        // -> MENU <-
+
+        // -> OPTION <-
+
+        // -> GAME <-
+
+        // -> CREDITS <-
+
+        //Autre
+        fpsFont = TTF_OpenFont("assets/font.ttf", 24);
+        fpsText = TTF_CreateText(textEngine, fpsFont, "FPS: 0", 20);
+        if (fpsText == nullptr) {
+            SDL_LogWarn(0,"failed to create text for fpsText, "FPS: 0", SDL_GetError());
+        }
+        fpsTimerID = SDL_AddTimer(250, TimerCallback, &shouldUpdateText);
+
+    }
+
+    //DESTRUCTEUR
+    ~GameApp() {
+        //renderer
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        // Timer
+        SDL_RemoveTimer(fpsTimerID);
+        //text et fonts
+        TTF_CloseFont(fpsFont);
+        TTF_DestroyText(fpsText);
+        //Texture
+    }
+
+};
