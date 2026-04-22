898dea3899ead689c12884baea997c16526da836
Louis <lpgauvin36@gmail.com>
2026-04-22 03:25:32 -0400

    Start Settlements components

---
+++ Settlements.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Louis-Philippe on 4/22/2026.
+//
+
+#include "Settlements.h"
---
+++ Settlements.h
@@ -0,0 +1,8 @@
+//
+// Created by Louis-Philippe on 4/22/2026.
+//
+
+#ifndef TOTALWAR2D_SETTLEMENTS_H
+#define TOTALWAR2D_SETTLEMENTS_H
+
+#endif //TOTALWAR2D_SETTLEMENTS_H
